ee10a5186e0ffb3ae1059b7ed634eb10e82117c2
AYATOSASAKI <ayaciijapan@gmail.com>
2021-12-17 17:37:38 +0900

    14th commit

--- pracPPrimeDeconv.php
+++ pracPPrimeDeconv.php
@@ -17,7 +17,34 @@
         <?php
             if (isset($_POST["primeF"])) {
                 $n1 = (int) $_POST["num"];
-                printf("%d =  <br>\n", $n1);
+                $prime_list = array();
+                $temp = $n1;
+                for ($i = 2 ; $i <= round($n**0.5) ; ++$i) {
+                    if($temp % $i == 0) {
+                        $cnt = 0;
+                        $prime_n_count = array();
+                        while($temp % $i == 0){
+                            $cnt += 1;
+                            $temp = round($temp / $i);
+                        }
+                        array_push($prime_n_count, $i, $cnt);
+                        array_push($prime_list, $prime_n_count);
+                    }
+                }
+                if ($tenp != 1){
+                    $arr = array();
+                    array_push($arr, $temp, $i);
+                    array_push($prime_list, $arr);
+                }
+                if (empty($prime_list)){
+                    $arr = array();
+                    array_push($arr, $n1, 1);
+                    array_push($prime_list, $arr);
+                }
+
+                for ($i = 0 ; $i < count($prime_list) ; ++$i) {
+                    print('$prime_list['.$i.'] = '. $prime_list[$i]." <br>\n");
+                }
             }
         ?>
     </body>

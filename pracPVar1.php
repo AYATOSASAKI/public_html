6b8adac251007d7fdace8b24cee68272eb32f821
AYATOSASAKI <ayaciijapan@gmail.com>
2021-12-17 15:46:32 +0900

    6th commit

---
+++ pracPVar1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Practice of PHP Variable 1 (title)</title>
+    </head>
+    <body>
+        <h1>Practice of PHP Variable 1</h1>
+
+            This is the first practice of PHP.
+
+        <ul>
+        <li>Hello world!</li>
+        <?php
+            $list = "<li>PHP is simple.</li>";
+            print $list;
+            $a = 1;
+            $b = 12.3;
+            $c = $a + $b;
+        ?>
+        <li>However, it has many commands</li>
+        </ul>
+            $c =
+        <?php
+            print $c;
+        ?>
+    </body>
+</html>

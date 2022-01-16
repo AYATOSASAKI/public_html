2b20bc5e2afd9b10042138e47018c5a3964f471c
AYATOSASAKI <65755265+AYATOSASAKI@users.noreply.github.com>
2022-01-17 01:09:06 +0900

    17

--- chat.php
+++ chat.php
@@ -49,7 +49,7 @@
                 let user_item = `<p class="user_item">${user_input}</p>`;
                 document.getElementById('chat_ul').insertAdjacentHTML('beforeend', user_item);
                 
-                let bot_hand = '<?php echo $abc; ?>';
+                let bot_hand = "<?php echo $abc; ?>";
                 let bot_item = `<p class="bot_item">${bot_hand}</p>`;
                 const bot = () => {
                     document.getElementById('chat_ul').insertAdjacentHTML('beforeend', bot_item);

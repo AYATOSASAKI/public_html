de252540b1df57dead3c7966d5b84df0a6994d9f
AYATOSASAKI <65755265+AYATOSASAKI@users.noreply.github.com>
2022-01-17 01:50:33 +0900

    37

--- chat.php
+++ chat.php
@@ -1,20 +1,5 @@
 <?php
-    $hand = $_GET["message"];
-    switch ($hand){
-        case "rock":
-            $hand = "paper";
-            break;
-        case "paper":
-            $hand = "scissor";
-            break;
-        case "scissor":
-            $hand = "rock";
-            break;
-        default:
-            $hand = "What is that ?";
-            break;
-        }
-    var_dump($hand);
+    $hand = "";
 ?>    
 
 <!DOCTYPE html>
@@ -61,3 +46,22 @@
         </script>
     </body>
 </html>
+
+<?php
+    $hand = $_GET["message"];
+    switch ($hand){
+        case "rock":
+            $hand = "paper";
+            break;
+        case "paper":
+            $hand = "scissor";
+            break;
+        case "scissor":
+            $hand = "rock";
+            break;
+        default:
+            $hand = "What is that ?";
+            break;
+        }
+    var_dump($hand);
+?>    

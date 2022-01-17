379963757e98dd5e072738503278c18203067c0a
AYATOSASAKI <ayaciijapan@gmail.com>
2022-01-17 15:36:33 +0900

    11

--- chat.js
+++ chat.js
@@ -3,11 +3,11 @@
 document.getElementById('form').onsubmit = function(event){
     event.preventDefault();
     const user_input = document.getElementById('form').message.value;
-    let user_item = `<p id="user_call" class="user_item">${user_input}</p>`;
+    let user_item = `<p class="user_item">${user_input}</p>`;
     document.getElementById('chat_ul').insertAdjacentHTML('beforeend', user_item);
 
     //let bot_hand = '<?php echo $hand; ?>';
-    let bot_item = `<p id="bot_response" class="bot_item">${user_input}</p>`;
+    let bot_item = `<p class="bot_item">${user_input}</p>`;
     const bot = () => {
         document.getElementById('chat_ul').insertAdjacentHTML('beforeend', bot_item);
         scrollBy(0, 1000);

8f3b22602a89c07425d6b14608bcb196af901e2f
AYATOSASAKI <ayaciijapan@gmail.com>
2022-01-17 10:34:58 +0900

    20

--- chat.php
+++ chat.php
@@ -51,7 +51,7 @@
                 let user_item = `<p class="user_item">${user_input}</p>`;
                 document.getElementById('chat_ul').insertAdjacentHTML('beforeend', user_item);
                 
-                let bot_hand = '<?php echo $reaction; ?>';
+                let bot_hand = '<?=$reaction?>';
                 let bot_item = `<p class="bot_item">${bot_hand}</p>`;
                 const bot = () => {
                     document.getElementById('chat_ul').insertAdjacentHTML('beforeend', bot_item);

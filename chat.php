ba7bc731b56446a704870628601a641b0bb16bd6
AYATOSASAKI <ayaciijapan@gmail.com>
2022-01-17 11:29:12 +0900

    27

--- chat.php
+++ chat.php
@@ -1,21 +1,47 @@
-<?php
-    if (isset($_POST["send_button"])) {
-        $hand = $_POST["message"];
-        switch ($hand){
-            case "rock":
-                $reaction = "paper";
-                break;
-            case "paper":
-                $reaction = "scissor";
-                break;
-            case "scissor":
-                $reaction = "rock";
-                break;
-            default:
-                $reaction = "What is that ?";
-                break;
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="chat.css" type="text/css">
+        <title>simple chatbot</title>
+    </head>
+    <body>
+        <section>
+            <div id="chat" class="chat"><br>
+                <ul id="chat_ul" class="chat_ul">
+                </ul><br><br><br><hr>
+            </div>
+
+            <div id="form_style" class="form_style">
+                <form action="#" method="POST" id="form" class="form">
+                    <input type="text" name="message" class="text">
+                    <input type="submit" name="send" value="send">      
+                </form>
+            </div>
+        </section>
+
+        <?php
+            if (isset($_POST["send_button"])) {
+                $hand = $_POST["message"];
+                switch ($hand){
+                    case "rock":
+                        $reaction = "paper";
+                        break;
+                    case "paper":
+                        $reaction = "scissor";
+                        break;
+                    case "scissor":
+                        $reaction = "rock";
+                        break;
+                    default:
+                        $reaction = "What is that ?";
+                        break;
+                    }
+                var_dump($hand);
+                var_dump($reaction);
             }
-        var_dump($hand);
-        var_dump($reaction);
-    }
-?>
+        ?>
+
+        <script src="chat.js" charset="utf-8"></script>
+    </body>
+</html>

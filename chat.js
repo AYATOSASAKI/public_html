abed87f9f6225f1c2a1a6b63c7747077ae30d0fa
AYATOSASAKI <ayaciijapan@gmail.com>
2022-01-17 15:33:11 +0900

    9

--- chat.js
+++ chat.js
@@ -6,6 +6,14 @@ document.getElementById('form').onsubmit = function(event){
     let user_item = `<p id="user_call" class="user_item">${user_input}</p>`;
     document.getElementById('chat_ul').insertAdjacentHTML('beforeend', user_item);
 
+    //let bot_hand = '<?php echo $hand; ?>';
+    let bot_item = `<p id="bot_response" class="bot_item">${user_input}</p>`;
+    const bot = () => {
+        document.getElementById('chat_ul').insertAdjacentHTML('beforeend', bot_item);
+        scrollBy(0, 1000);
+    };
+    setTimeout(bot, 300);
+
     form.reset();
     scrollBy(0, 1000);
 

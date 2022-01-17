205469b5c565f006d8d6599bb0731e085c660444
AYATOSASAKI <ayaciijapan@gmail.com>
2022-01-17 11:07:03 +0900

    25

--- chat.php
+++ chat.php
@@ -21,27 +21,7 @@
         </section>
 
          
-        <?php
-            if (isset($_POST["send_button"])) {
-                $hand = $_POST["message"];
-                switch ($hand){
-                    case "rock":
-                        $reaction = "paper";
-                        break;
-                    case "paper":
-                        $reaction = "scissor";
-                        break;
-                    case "scissor":
-                        $reaction = "rock";
-                        break;
-                    default:
-                        $reaction = "What is that ?";
-                        break;
-                    }
-                var_dump($hand);
-                var_dump($reaction);
-            }
-        ?>
+
 
         <script src="chat.js" charset="utf-8"></script>
 

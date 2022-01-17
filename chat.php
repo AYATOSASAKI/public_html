7708a4a0569951134b2cb716f24d1e06f1bbef53
AYATOSASAKI <ayaciijapan@gmail.com>
2022-01-17 09:34:43 +0900

    2

--- chat.php
+++ chat.php
@@ -21,22 +21,24 @@
         </section>
 
         <?php
-            $hand = $_POST["message"];
-            switch ($hand){
-                case "rock":
-                    $hand = "paper";
-                    break;
-                case "paper":
-                    $hand = "scissor";
-                    break;
-                case "scissor":
-                    $hand = "rock";
-                    break;
-                default:
-                    $hand = "What is that ?";
-                    break;
-                }
-            var_dump($hand);
+            if (isset($_POST["send"])) {
+                $hand = $_POST["message"];
+                switch ($hand){
+                    case "rock":
+                        $hand = "paper";
+                        break;
+                    case "paper":
+                        $hand = "scissor";
+                        break;
+                    case "scissor":
+                        $hand = "rock";
+                        break;
+                    default:
+                        $hand = "What is that ?";
+                        break;
+                    }
+                var_dump($hand);
+            }
         ?>
 
         <script>

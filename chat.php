da0a22bb858c25e5400faaa04c5c5557f4a6290d
AYATOSASAKI <ayaciijapan@gmail.com>
2022-01-17 15:38:33 +0900

    12

--- chat.php
+++ chat.php
@@ -41,7 +41,28 @@
                 var_dump($reaction);
             }
         ?>
-        <script src="chat.js" charset="utf-8"></script>
 
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js">
+            'use strict';
+            document.getElementById('form').onsubmit = function(event){
+                event.preventDefault();
+                const user_input = document.getElementById('form').message.value;
+                let user_item = `<p class="user_item">${user_input}</p>`;
+                document.getElementById('chat_ul').insertAdjacentHTML('beforeend', user_item);
+            
+                //let bot_hand = '<?php echo $hand; ?>';
+                let bot_item = `<p class="bot_item">${user_input}</p>`;
+                const bot = () => {
+                    document.getElementById('chat_ul').insertAdjacentHTML('beforeend', bot_item);
+                    scrollBy(0, 1000);
+                };
+                setTimeout(bot, 300);
+            
+                form.reset();
+                scrollBy(0, 1000);
+            
+                //event.off();
+            };
+        </script>
     </body>
 </html>

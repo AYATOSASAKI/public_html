75e4c759553fabcae61655cb649004e08d584e6b
AYATOSASAKI <ayaciijapan@gmail.com>
2022-01-17 11:22:39 +0900

    26

--- chat.js
+++ chat.js
@@ -5,6 +5,7 @@ document.getElementById('form').onsubmit = function(event){
     let user_item = `<p class="user_item">${user_input}</p>`;
     document.getElementById('chat_ul').insertAdjacentHTML('beforeend', user_item);
     
+    //let bot_hand = '<?php echo $hand; ?>';
     let bot_item = `<p class="bot_item">${user_input}</p>`;
     const bot = () => {
         document.getElementById('chat_ul').insertAdjacentHTML('beforeend', bot_item);
--- chat.php
+++ chat.php
@@ -1,29 +1,21 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="chat.css" type="text/css">
-        <title>simple chatbot</title>
-    </head>
-    <body>
-        <section>
-            <div id="chat" class="chat"><br>
-                <ul id="chat_ul" class="chat_ul">
-                </ul><br><br><br><hr>
-            </div>
-
-            <div id="form_style" class="form_style">
-                <form action="chat.php" method="POST" id="form" class="form">
-                    <input type="text" name="message" class="text">
-                    <input type="submit" name="send_button" value="send">      
-                </form>
-            </div>
-        </section>
-
-         
-
-
-        <script src="chat.js" charset="utf-8"></script>
-
-    </body>
-</html>
+<?php
+    if (isset($_POST["send_button"])) {
+        $hand = $_POST["message"];
+        switch ($hand){
+            case "rock":
+                $reaction = "paper";
+                break;
+            case "paper":
+                $reaction = "scissor";
+                break;
+            case "scissor":
+                $reaction = "rock";
+                break;
+            default:
+                $reaction = "What is that ?";
+                break;
+            }
+        var_dump($hand);
+        var_dump($reaction);
+    }
+?>

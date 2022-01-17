b3b8590808e0980000396d5f5f19c037f374110e
AYATOSASAKI <ayaciijapan@gmail.com>
2022-01-17 15:54:59 +0900

    18

--- chat.php
+++ chat.php
@@ -46,7 +46,7 @@
         <script>
             'use strict';
             document.getElementById('form').onsubmit = function(event){
-                event.stopPropagation();
+                event.preventDefault();
                 const user_input = document.getElementById('form').message.value;
                 let user_item = `<p class="user_item">${user_input}</p>`;
                 document.getElementById('chat_ul').insertAdjacentHTML('beforeend', user_item);
@@ -63,8 +63,8 @@
                 form.reset();
                 scrollBy(0, 1000);
             
-                event.off();
             };
+            event.off();
         </script>
     </body>
 </html>

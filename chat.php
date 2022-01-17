9b60685c9060b78b6383ab8e61c5aad39ea904b7
AYATOSASAKI <ayaciijapan@gmail.com>
2022-01-17 09:41:29 +0900

    4

--- chat.php
+++ chat.php
@@ -20,8 +20,9 @@
             </div>
         </section>
 
+         
         <?php
-            if (isset($_POST["send"])) {
+            /*if (isset($_POST["send"])) {
                 $hand = $_POST["message"];
                 switch ($hand){
                     case "rock":
@@ -39,7 +40,7 @@
                     }
                 var_dump($hand);
                 var_dump($reaction);
-            }
+            }*/
         ?>
 
         <script>
@@ -50,8 +51,7 @@
                 let user_item = `<p class="user_item">${user_input}</p>`;
                 document.getElementById('chat_ul').insertAdjacentHTML('beforeend', user_item);
                 
-                let bot_hand = '<?php echo $reaction; ?>';
-                let bot_item = `<p class="bot_item">${bot_hand}</p>`;
+                let bot_item = `<p class="bot_item">${user_input}</p>`;
                 const bot = () => {
                     document.getElementById('chat_ul').insertAdjacentHTML('beforeend', bot_item);
                     scrollBy(0, 1000);

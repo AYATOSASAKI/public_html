18c4ed93f3924992a0401e6b7f26153f8ecf4dc7
AYATOSASAKI <ayaciijapan@gmail.com>
2022-01-17 10:42:48 +0900

    23

--- chat.php
+++ chat.php
@@ -43,30 +43,5 @@
             }
         ?>
 
-        <script>
-            'use strict';
-            let bot_hand = '<?php echo $hand; ?>';
-            document.getElementById('form').onsubmit = function(event){
-                event.preventDefault();
-                const user_input = document.getElementById('form').message.value;
-                let user_item = `<p class="user_item">${user_input}</p>`;
-                document.getElementById('chat_ul').insertAdjacentHTML('beforeend', user_item);
-                
-                let bot_item = `<p class="bot_item">${bot_hand}</p>`;
-                const bot = () => {
-                    document.getElementById('chat_ul').insertAdjacentHTML('beforeend', bot_item);
-                    scrollBy(0, 1000);
-                };
-                setTimeout(bot, 300);
-
-                form.reset();
-                scrollBy(0, 1000);
-            };
-        </script>
-
-        <?php
-            var_dump($hand);
-            var_dump($reaction);
-        ?>
     </body>
 </html>

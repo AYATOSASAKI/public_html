5dd1a806f16e53bfe9a598519addc97f2d37a9eb
AYATOSASAKI <ayaciijapan@gmail.com>
2022-01-17 15:02:15 +0900

    3

--- chat.js
+++ chat.js
@@ -1,6 +1,6 @@
 'use strict';
 document.getElementById('form').onsubmit = function(event){
-    event.preventDefault();
+    //event.preventDefault();
     const user_input = document.getElementById('form').message.value;
     let user_item = `<p class="user_item">${user_input}</p>`;
     document.getElementById('chat_ul').insertAdjacentHTML('beforeend', user_item);

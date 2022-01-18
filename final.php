d2f2e71a2b9a213cb04b6c642044867c896aeecd
AYATOSASAKI <65755265+AYATOSASAKI@users.noreply.github.com>
2022-01-18 14:02:39 +0900

    final

---
+++ final.php
@@ -0,0 +1,50 @@
+<?php 
+    function sv(){
+        echo 'Bot :    Your text length is';
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="chat.css" type="text/css">
+        <title>simple chatbot</title>
+    </head>
+    <body>
+        <section>
+            <div id="chat" class="chat"><br>
+                <ul id="chat_ul" class="chat_ul">
+                </ul><br><br><br><hr>
+            </div>
+
+            <div id="form_style" class="form_style">
+                <form method="POST" id="form" class="form">
+                    <input type="text" name="message" class="text">
+                    <input type="submit" name="send_button" value="send">      
+                </form>
+            </div>
+        </section>
+
+        <script>
+            'use strict';
+            document.getElementById('form').onsubmit = function(event){
+                event.preventDefault();
+                const user_input = document.getElementById('form').message.value;
+                let user_item = `<p class="user_item">You :    ${user_input}</p>`;
+                document.getElementById('chat_ul').insertAdjacentHTML('beforeend', user_item);
+
+                let bot_res = "<?php sv(); ?>";
+                let word_count = user_input.length;
+                let bot_item = `<p class="bot_item">${bot_res} ${word_count}.</p>`;
+                const bot = () => {
+                    document.getElementById('chat_ul').insertAdjacentHTML('beforeend', bot_item);
+                    scrollBy(0, 1000);
+                };
+                setTimeout(bot, 300);
+                form.reset();
+                scrollBy(0, 1000);
+            };
+        </script>
+    </body>
+</html>
